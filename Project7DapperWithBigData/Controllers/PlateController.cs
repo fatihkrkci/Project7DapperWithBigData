c4c51d74f5a373d6f197097a858bd2eb9749ee29
Fatih Kürekçi <fatihkr02@gmail.com>
2025-01-11 12:16:36 +0300

    added search in PlateList

--- Project7DapperWithBigData/Controllers/PlateController.cs
+++ Project7DapperWithBigData/Controllers/PlateController.cs
@@ -13,15 +13,27 @@ namespace Project7DapperWithBigData.Controllers
             _plateRepository = plateRepository;
         }
 
-        public async Task<IActionResult> PlateList(int? page)
+        public async Task<IActionResult> PlateList(int? page, string searchTerm)
         {
             int pageSize = 10;
             int pageNumber = page ?? 1;
 
             var allPlates = await _plateRepository.GetAllPlateAsync();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                allPlates = allPlates
+                    .Where(p => p.Plate.Contains(searchTerm, StringComparison.OrdinalIgnoreCase) ||
+                                (p.Title != null && p.Title.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)) ||
+                                (p.Brand != null && p.Brand.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)))
+                    .ToList();
+            }
+
             var pagedPlates = allPlates.ToPagedList(pageNumber, pageSize);
 
+            ViewBag.SearchTerm = searchTerm; // Arama terimini View'e göndermek için
             return View(pagedPlates);
         }
+
     }
 }
